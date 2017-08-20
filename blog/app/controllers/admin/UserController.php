cd67fb0ab7c4e0afbdd82c5482ec34d6f2fbf4f0
Alejandro Sulbaran <sulbaran.kun@gmail.com>
2017-08-20 12:50:40 -0400

    Agregando usuarios

--- blog/app/controllers/admin/UserController.php
+++ blog/app/controllers/admin/UserController.php
@@ -4,6 +4,8 @@ namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Sirius\Validation\Validator;
 
 class UserController extends BaseController{
     public function getIndex(){
@@ -11,5 +13,41 @@ class UserController extends BaseController{
         return $this->render('admin/users.twig',[
             'users' => $user
         ]);
+    }
+
+    public function getCreate(){
+
+        return $this->render('admin/insert-user.twig',[]);
+
+    }
+
+    public function postCreate(){
+
+        $errors = [];
+        $result =false;
+        $validator =new Validator();
+        $validator->add('name','required');
+        $validator->add('email','required');
+
+        if ($validator->validate($_POST)){
+
+            $user = new User([
+                'name'     => $_POST['name'],
+                'email'   => $_POST['email'],
+                'password' => $_POST['password']
+            ]);
+            $user->save();
+
+            $result = true;
+        }else{
+            $errors = $validator->getMessages();
+        }
+
+        return $this->render('admin/insert-user.twig',[
+            'result'    =>  $result,
+            'errors'    =>  $errors
+        ]);
+
+
     }
 }
